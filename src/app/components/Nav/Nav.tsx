b9a131e0dbdaac73807efc1d573bd3a15af41fa8
nobodyw <michael.wong@epitech.eu>
2025-05-20 08:49:23 +0200

    add scrolling navbar effect + add other logo white

--- src/app/components/Nav/Nav.tsx
+++ src/app/components/Nav/Nav.tsx
@@ -1,20 +1,53 @@
-import styles from './Nav.module.scss';
-import Image from 'next/image';
-import logoBlack from '../../../../public/assets/Logo/logoBlack.svg';
-import bmClose from '../../../../public/assets/Nav/bmClose.svg';
+"use client";
+import styles from "./Nav.module.scss";
+import Image from "next/image";
+import logoBlack from "../../../../public/assets/Logo/logoBlack.svg";
+import logoWhite from "../../../../public/assets/Logo/logoWhite.svg";
+import bmClose from "../../../../public/assets/Nav/bmClose.svg";
+import bmCloseWhite from "../../../../public/assets/Nav/bmCloseWhite.svg";
+import { useScroll, useMotionValueEvent } from "framer-motion";
+import { useState } from "react";
+import { navFont } from "@/app/fonts";
+
+
 export default function Nav() {
+  const { scrollY } = useScroll();
+  const [scrolled, setScrolled] = useState(false);
+
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    if (latest > 0) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  });
+
   return (
-    <div className={`navbar navFont ${styles.nav}`}>
-        <div className={styles.burgerMenu}>
-            <Image src={bmClose} alt="Open Burger Menu"/>
-            <span>Menu</span>
-        </div>
-        <div className={styles.logo}>
-            <Image src={logoBlack} alt="Logo Orchidea Beach"/>
-        </div>
-        <div className={styles.contact}>
-            <span>Contact</span>
-        </div>
+    <div
+      className={`navbar ${navFont.className} ${styles.nav} ${
+        scrolled ? styles.navScrolled : ""
+      }`}
+    >
+      <div className={styles.burgerMenu}>
+        {scrolled ? (
+          <Image src={bmCloseWhite} alt="Open Burger Menu" />
+        ) : (
+          <Image src={bmClose} alt="Open Burger Menu" />
+        )}
+        <span>Menu</span>
+      </div>
+
+      <div className={styles.logo}>
+        {scrolled ? (
+          <Image src={logoWhite} alt="Logo Orchidea Beach" />
+        ) : (
+          <Image src={logoBlack} alt="Logo Orchidea Beach" />
+        )}
+      </div>
+
+      <div className={styles.contact}>
+        <span>Contact</span>
+      </div>
     </div>
   );
 }
