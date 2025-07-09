bc6a71c9d1e30bb101747ca887dc30e16b51e2a0
nobodyw <michael.wong@epitech.eu>
2025-07-09 19:15:56 +0200

    Désactivate scroll automatique

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Nav from "./components/Nav/Nav";
 import HeroBackground from "./components/HeroBackground/HeroBackground";
 import Contact from "./components/Contact/Contact";
@@ -7,24 +7,24 @@ import Experience from "./components/Experience/Experience";
 import Service from "./components/Services/Service";
 import Excursion from "./components/Excursion/Excursion";
 import Footer from "./components/Footer/Footer";
-import SectionScroll from "./components/SectionScroll";
+// import SectionScroll from "./components/SectionScroll";
 
 export default function Home() {
-  const [showSectionScroll, setShowSectionScroll] = useState(false);
+  // const [showSectionScroll, setShowSectionScroll] = useState(false);
 
   useEffect(() => {
-    const handleResize = () => {
-      setShowSectionScroll(window.innerWidth > 1041);
-    };
-    handleResize();
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
+    // const handleResize = () => {
+    //   setShowSectionScroll(window.innerWidth > 1041);
+    // };
+    // handleResize();
+    // window.addEventListener("resize", handleResize);
+    // return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return (
     <div id="main">
       <Nav />
-      {showSectionScroll && <SectionScroll offset={120} />}
+      {/* {showSectionScroll && <SectionScroll offset={120} />} */}
       <section id="section-0"><HeroBackground /></section>
       <section id="section-1"><Contact /></section>
       <section id="section-2"><Experience /></section>
